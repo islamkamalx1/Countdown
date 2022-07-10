4c2b141eed454b1cbdccd1a418a867e8c1912263
Islam Kamal <islamkamalx1>
2022-07-10 16:26:13 +0200

    update

--- app.js
+++ app.js
@@ -22,11 +22,51 @@ const weekdays = [
   "Saturday",
 ];
 
+
+document.body.innerHTML = `<section class="section-center">
+<article class="gift-img">
+  <img src="./gift.jpeg" alt="gift photo">
+</article>
+<article class="gift-info">
+  <h3>old iphone giveaway</h3>
+  <h4 class="giveaway">
+    giveaway ends on sunday, 15 september 2022, 8:00am
+  </h4>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quaerat alias nam optio sapiente itaque. Asperiores amet autem debitis nemo?</p>
+  <div class="deadline">
+    <div class="deadline-format">
+    <div>
+    <h4 class="days">34</h4>
+    <span>days</span>
+    </div>
+    </div>
+    <div class="deadline-format">
+    <div>
+    <h4 class="hours">34</h4>
+    <span>hours</span>
+      </div>
+      </div>
+      <div class="deadline-format">
+      <div>
+        <h4 class="mins">34</h4>
+        <span>mins</span>
+        </div>
+        </div>
+        <div class="deadline-format">
+        <div>
+        <h4 class="sec">34</h4>
+        <span>sec</span>
+        </div>
+        </div>
+  </div>
+</article>
+</section>`;
+
 const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2022,6,10,16,7,59);
+let futureDate = new Date(2022,8,14,23,59,59);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
